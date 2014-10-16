c0af47d807b548b059f9597c935d6085ed7cec57
Liam Morley <liam@carpeliam.com>
2014-10-16 14:13:35 -0400

    add route for shared patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -4,6 +4,10 @@ class PatientsController < ApplicationController
   RACE_NAME_MAP={'1002-5' => 'American Indian or Alaska Native','2028-9' => 'Asian','2054-5' => 'Black or African American','2076-8' => 'Native Hawaiian or Other Pacific Islander','2106-3' => 'White','2131-1' => 'Other'}
   ETHNICITY_NAME_MAP={'2186-5'=>'Not Hispanic or Latino', '2135-2'=>'Hispanic Or Latino'}
 
+  def index
+    render :json => Record.where(is_shared: true)
+  end
+
   def update
     patient = Record.by_user(current_user).find(params[:id]) # FIXME: will we have an ID attribute on server side?
     update_patient(patient)
