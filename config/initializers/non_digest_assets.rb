a79f1d37725a67bf71431a980afd7adc2535dfec
Chris Tohline <ctohline@mitre.org>
2017-02-06 10:26:26 -0500

    fix for jquery.dataTables images not working correctly on production

--- config/initializers/non_digest_assets.rb
+++ config/initializers/non_digest_assets.rb
@@ -1,1 +1,1 @@
-NonStupidDigestAssets.whitelist = [/fontawesome/, /FontAwesome/, 'loading.gif', 'Rabbit-96-blue.png']
+NonStupidDigestAssets.whitelist = [/fontawesome/, /FontAwesome/, 'loading.gif', 'Rabbit-96-blue.png', 'sort_asc.png', 'sort_both.png', 'sort_desc.png']
